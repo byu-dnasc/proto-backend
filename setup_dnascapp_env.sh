525aa9e6b6427c601617218bfc8cad5713c7d14e
Ammon Knaupp <aknaupp@smrt.rc.byu.edu>
2023-09-09 13:51:02 -0600

    Bug fix

--- setup_dnascapp_env.sh
+++ setup_dnascapp_env.sh
@@ -1,9 +1,10 @@
-conda="/home/dnascapp/miniconda3/condabin/conda"
+conda=/home/dnascapp/miniconda3/bin/conda
 $conda create --name dnascapp_env
-$conda activate dnascapp_env
+# important: conda activate is not available non-interactively
+# therefore, use source to activate the environment
+source /home/dnascapp/miniconda3/bin/activate dnascapp_env
 $conda install python
 $conda install sqlite
 $conda install openssl
 $conda install urllib3
 python -m pip install globus_sdk
-$conda deactivate
